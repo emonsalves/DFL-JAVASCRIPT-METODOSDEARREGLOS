9c23eb53e49c404b64ea8759c1a0e14a992200c1
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-07-28 13:50:29 -0400

    Feat: Script with functions ready

---
+++ assets/js/clase.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const servicios = [
+    { id: 1, nombre: "Agua" },
+    { id: 2, nombre: "Electricidad" },
+    { id: 3, nombre: "Gas" },
+    { id: 4, nombre: "Internet" },
+    { id: 5, nombre: "Cable Tv" },
+    ];
+
+const paso = document.querySelector(".servicios")
